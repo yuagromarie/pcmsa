7941d60221410f78907969dc57aab680b85cb06d
ivymay <ivymay.bulala@yahoo.com>
2013-03-31 12:35:10 -0500

    DeviceController Updated

--- OwlingApp/grails-app/controllers/owlingapp/DeviceController.groovy
+++ OwlingApp/grails-app/controllers/owlingapp/DeviceController.groovy
@@ -1,6 +1,17 @@
 package owlingapp
 
-class DeviceController {
+import grails.plugins.springsecurity.Secured
 
-    def index() { }
+@Secured(['ROLE_CONSUMER'])
+class DeviceController {
+	
+	
+	def index={ }
+		
+   	def list(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        [deviceInstanceList: Device.list(params), deviceInstanceTotal:  Device.count()]
+    }
+
+	
 }
